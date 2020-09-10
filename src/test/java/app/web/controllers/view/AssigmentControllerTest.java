06584c87ff7bad580cd3d240e50b791496967298
rusevradoslav <radorusev98@gmail.com>
2020-09-10 12:50:37 +0300

    Add Tests For Controllers And 87% code coverage

---
+++ src/test/java/app/web/controllers/view/AssigmentControllerTest.java
@@ -0,0 +1,17 @@
+package app.web.controllers.view;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AssigmentControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
